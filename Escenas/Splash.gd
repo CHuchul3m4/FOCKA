6bba43a964d50487764260dbeafde7073598db15
jerecraft-YT <contactojerecraft@gmail.com>
2026-07-21 20:49:20 -0500

    todo good

--- Escenas/Splash.gd
+++ Escenas/Splash.gd
@@ -7,6 +7,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+@warning_ignore("unused_parameter")
 func _process(delta: float):
 	if Input.is_action_just_pressed("UI_CLICK"):
 		fadeout()
--- Escenas/label.gd
+++ Escenas/label.gd
@@ -5,5 +5,6 @@ extends Label
 func _ready() -> void:
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+@warning_ignore("unused_parameter")
 func _process(delta: float) -> void:
 	self.text  = "fps " + str(Engine.get_frames_per_second())
